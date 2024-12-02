a516bcc08245faf11707b193f9159cec367c2993
Ваше Имя <you@example.com>
2024-12-02 17:04:04 +0300

    005

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bot import send
 app = FastAPI()
 
 
-@app.get("/")
+@app.post("/")
 async def read_root(message: sheme.Base):
     await send(message.message)
     return JSONResponse(status_code=200, content={})
