35ced235b44e90f8f0537b8dbe24b0b5dd42bbbb
Denmais <malichdenis09@mail.ru>
2024-12-18 16:31:44 +0300

    new

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bot import send
 app = FastAPI()
 
 
-@app.post("/")
+@app.post("/message")
 async def read_root(message: sheme.Base):
     await send(message.message)
     return JSONResponse(status_code=200, content={})
